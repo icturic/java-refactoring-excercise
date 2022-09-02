5edc38d0820a2df73079ca310a4e32db5bc94621
icturic <112683415+icturic@users.noreply.github.com>
2022-09-02 19:12:31 +0300

    Update Translation.java

--- src/main/java/com/mcserby/training/refactoring/Translation.java
+++ src/main/java/com/mcserby/training/refactoring/Translation.java
@@ -1,22 +1,15 @@
 package com.mcserby.training.refactoring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class Translation {
 
     private final List<String> values;
     private final Locale locale;
 
-    public Translation(List<String> values, Locale locale) {
-        this.values = values;
-        this.locale = locale;
-    }
-
-    public List<String> getValues() {
-        return values;
-    }
-
-    public Locale getLocale() {
-        return locale;
-    }
 }
